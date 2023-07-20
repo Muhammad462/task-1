446ec23c610a88c4cc57b077c44c7b82f7862918
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-20 14:20:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Task 1 : store person name in variable and print message to that person 
+let namee = "sherry";
+console.log(`Hello ${namee}, would you like to learn some python today?`);
---
+++ index.ts
@@ -0,0 +1,4 @@
+// Task 1 : store person name in variable and print message to that person 
+let namee  = "sherry";
+console. log(`Hello ${namee}, would you like to learn some python today?`)
+
